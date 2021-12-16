7a306074a19bff2cc722322686998a1f14872f1e
DOBIN <ff09560810@gmail.com>
2021-12-16 21:45:53 +0900

    Initial commit

---
+++ src/main/java/maeil/maeilproject/MaeilProjectApplication.java
@@ -0,0 +1,13 @@
+package maeil.maeilproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaeilProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MaeilProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/maeil/maeilproject/MaeilProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package maeil.maeilproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaeilProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
