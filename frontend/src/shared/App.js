f91382e1952f55ac9c314a123f967e0536bd1b23
min-9 <mg07315@gmail.com>
2021-12-18 21:10:47 +0900

    Create 'frontend' directory and set up React project

---
+++ frontend/src/shared/App.js
@@ -0,0 +1,11 @@
+import { Route, Routes } from 'react-router-dom';
+import { About, Home } from '../pages';
+
+export default function App() {
+  return (
+    <Routes>
+      <Route exact path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+    </Routes>
+  );
+}
