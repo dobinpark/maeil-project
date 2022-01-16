901bf395c14a4ad1dc9464ddfd85ae38a36db62c
ff502 <ff09560810@gmail.com>
2022-01-16 23:11:43 +0900

    maeil-project

---
+++ src/main/java/maeil/maeilproject/model/Role.java
@@ -0,0 +1,24 @@
+package maeil.maeilproject.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @ManyToMany(mappedBy = "roles")
+    private List<User> users;
+}
